14a40e23c9edd0d10ff213e70b3dc389fd1697dd
pravin7878 <pravinmimrot648@gmail.com>
2025-09-06 07:39:45 +0530

    env variable error fixed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const productRouter = require("./routes/product")
 const cors = require("cors")
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080 
 
 app.use(cors("*"))
 app.use(express.json())
